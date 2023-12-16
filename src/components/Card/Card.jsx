f0397980e079008fd38cc844ba46ba257752ddae
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-16 20:53:10 +0300

    Structure card faces and style them

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,18 @@
-import "./Card.css"
+import "./Card.css";
 
 export default function Card() {
     return (
         <div className="card">
             <div className="card-faces">
-                <div className="front-face">Hey this is the front of the card!</div>
-                <div className="back-face">Hey this is the front of the card!</div>
+                <div className="back-face">
+                    <p>Once upon a time there was a wicked card!</p>
+                </div>
+                <div className="front-face">
+                    <h4 className="card-power">22</h4>
+                    <h3 className="card-name">Ferhat Khan</h3>
+                </div>
             </div>
-            <div class="card-buttons">
+            <div className="card-buttons">
                 <button className="rotate-left">Turn left</button>
                 <button className="purchase">Purchase</button>
                 <button className="rotate-right">Turn right</button>
