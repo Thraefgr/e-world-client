e051842162eef460c00b2eccc39d41ce4ecaa43c
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-17 16:46:53 +0300

    Make cards section fetch data from server and render it on screen

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,23 @@
 import "./Cards.css";
+import Card from "../Card/Card.jsx";
+import { useEffect, useState } from "react";
 
-export default function Cards() {
+export default function Cards(sessionToken) {
+    const [cardList, setCardList] = useState([]);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/", {
+            headers: {
+                "Authorization": ``
+            }
+        })
+        .then(response => response.json())
+        .then(data => setCardList(data))
+    }, [])
+
     return (
-        <section className="Cards">These are the cards!</section>
+        <section className="Cards">
+            {cardList.map(card => <Card key={card.id} card={card} />)}
+        </section>
     )
 }
