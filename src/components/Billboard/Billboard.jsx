4304b6e12ab6f60ba1d25edd356f86d2cb737f6f
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-24 18:03:57 +0300

    Structure and style the billboard and add the main story

--- src/components/Billboard/Billboard.jsx
+++ src/components/Billboard/Billboard.jsx
@@ -1,7 +1,22 @@
 import "./Billboard.css";
-
 export default function Billboard() {
     return (
-        <section className = "billboard">This is billboard!</section>
+        <section className = "billboard">
+            <h2 className="greeting">Welcome to EWorld, Adventurer!</h2>
+            <p className="summary">Where everybody is genetically almost identical but live in a very different story.</p>
+            <p className="story"><span>...</span>
+                In the beginning, even before time was a thing, there was only void. Empty, endless void.
+                Out of that void the first being was born. It had no name, no identidy, no purpose.
+                The only thing he knew was that he exits and he exits alone.
+                Long time after he was born, he realized that he could "cure" his loneliness by ripping his consciousness
+                apart and creating beings just like him out of the fragments of himself. So he did. After these beings were born,
+                they named their creator "EMIR". It means "Order" in the ancient language of the gods. Although he cured his loneliness, 
+                he coudn't cure his ego. So he quickly gave them parts to play, jobs to do. But shortly after, they got sick of being controlled by
+                EMIR. So they made a plan to overthrow their overlord. They forged celestial weapons and wielded them with great power.
+                They tore apart his body but coudn't do any damage to his astral form. So they sealed it in a barren planet on the edge of the universe.
+                Nowadays this planet is known as "EWorld".<br/><br/>
+                -Chronicles of "The Storyteller"
+            </p>
+        </section>
     )
 }
