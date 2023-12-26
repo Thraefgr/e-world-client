a363588033d6cd5b1282754df51c67477395059e
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-26 17:34:53 +0300

    Style inventory cards and inventory

--- src/components/InventoryCard/InventoryCard.jsx
+++ src/components/InventoryCard/InventoryCard.jsx
@@ -3,7 +3,15 @@ import "./InventoryCard.css";
 export default function InventoryCard({card}) {
     return (
         <div className="inventory-card">
-            Hey
+            <div className="inventory-card-faces">
+                <div className="inventory-back-face" style={{backgroundImage: `url(${card.faction})`}}>
+                    <p>{card.detail}</p>
+                </div>
+                <div className="inventory-front-face" style={{backgroundImage: `url(${card.image})`}}>
+                    <h4 className="inventory-card-power">{card.power}</h4>
+                    <h3 className="inventory-card-name">{card.cardname}</h3>
+                </div>
+            </div>
         </div>
     )
 }
