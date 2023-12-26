fbcde512ada292b9a49a00a170b37d5c108aacdb
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-26 19:13:30 +0300

    Add a basic route protection to /inventory

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,11 +1,11 @@
 import "./Inventory.css";
-import { useState, useEffect } from "react";
-import InventoryCard from "../../components/InventoryCard/InventoryCard.jsx";
+import { useState, useEffect} from "react";
+import { Navigate } from "react-router-dom";
 import StoryBox from "../../components/StoryBox/StoryBox.jsx";
+
 
 export default function Inventory({userName, token}) {
     const [ownedCards, setOwnedCards] = useState([]);
-
     useEffect(() => {
         fetch(`http://127.0.0.1:8000/inventory/${userName}/`,{
             headers: {"Authorization": `${token}`}
@@ -14,7 +14,9 @@ export default function Inventory({userName, token}) {
         .then(data => setOwnedCards(data))
         .catch(err => console.log(err));
     }, [])
-
+    if (token === null || token === "") {
+        return <Navigate to="/"/>;
+    }
     return (
         <main className = "inventory">
             {
