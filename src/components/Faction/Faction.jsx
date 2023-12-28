82aaa84920e1c56d732e28c100d4ae362b9fa7d4
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-28 16:48:25 +0300

    Style faction and factions component

--- src/components/Faction/Faction.jsx
+++ src/components/Faction/Faction.jsx
@@ -2,9 +2,8 @@ import "./Faction.css";
 
 export default function Faction({faction}) {
     return (
-        <div className="faction">
-            <img src={faction.flag} alt=""/>
-            <div>
+        <div className="faction" style={{backgroundImage: `url(${faction.flag})`}}>
+            <div className="faction-info">
                 <h2>{faction.faction_name}</h2>
                 <p>{faction.history}</p>
             </div>
