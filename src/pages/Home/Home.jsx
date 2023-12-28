58f6bbe435fd3a42eaf76fb99b7823d01824361e
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-28 18:19:05 +0300

    Add a welcome section to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import "./Home.css";
 import Cards from "../../components/Cards/Cards.jsx";
+import Welcome from "../../components/Welcome/Welcome.jsx";
 
 export default function Home({token, userName}) {
     return (
         <main className="home">
+            <Welcome />
             <Cards token={token} userName={userName}/>
         </main>
     )
