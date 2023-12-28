b60bd115de46172bb29ce50c2c78be98f8a86703
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-28 17:36:10 +0300

    Move billboard to world page and style faction and factions a bit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
 import "./Home.css";
-import Billboard from "../../components/Billboard/Billboard.jsx";
 import Cards from "../../components/Cards/Cards.jsx";
 
 export default function Home({token, userName}) {
     return (
         <main className="home">
-            <Billboard />
             <Cards token={token} userName={userName}/>
         </main>
     )
--- src/pages/World/World.jsx
+++ src/pages/World/World.jsx
@@ -1,9 +1,11 @@
 import "./World.css";
 import Factions from "../../components/Factions/Factions.jsx";
+import Billboard from "../../components/Billboard/Billboard.jsx";
 
 export default function World() {
     return (
         <main className="World">
+            <Billboard />
             <Factions />
         </main>
     )
