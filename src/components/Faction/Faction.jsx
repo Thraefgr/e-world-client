12156a4ab1fcf9baf4dff554610826058cfaa0b1
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-12-28 14:42:01 +0300

    Fetch each faction from api and map it to faction component

--- src/components/Faction/Faction.jsx
+++ src/components/Faction/Faction.jsx
@@ -2,6 +2,12 @@ import "./Faction.css";
 
 export default function Faction({faction}) {
     return (
-        <div className="faction"></div>
+        <div className="faction">
+            <img src={faction.flag} alt=""/>
+            <div>
+                <h2>{faction.faction_name}</h2>
+                <p>{faction.history}</p>
+            </div>
+        </div>
     )
 }
--- src/components/Factions/Factions.jsx
+++ src/components/Factions/Factions.jsx
@@ -1,9 +1,16 @@
 import "./Factions.css";
 import Faction from "../Faction/Faction.jsx";
+import { useEffect, useState } from "react";
 export default function Factions() {
+    const [factions, setFactions] = useState([]);
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/factions/")
+        .then(res => res.json())
+        .then(data => setFactions(data))
+    })
     return (
         <section className="factions">
-            <h2>Factions</h2>
+            {factions.map(faction => <Faction key={faction.id} faction={faction}/>)}
         </section>
     )
 }
