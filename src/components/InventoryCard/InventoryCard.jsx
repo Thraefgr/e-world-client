e74cdcb6de37c0a8edc78f97cfdea06c0e60d50e
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-01-02 18:11:30 +0300

    Fix a bug where inventory was requesting data from api non stop

--- src/components/InventoryCard/InventoryCard.jsx
+++ src/components/InventoryCard/InventoryCard.jsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./InventoryCard.css";
 
 export default function InventoryCard({card}) {
     const [url, setUrl] = useState("");
-    fetch(card.image)
-    .then(response => response.blob())
-    .then(blob => {
-      const localUrl = window.URL.createObjectURL(blob);
-      setUrl(localUrl);
-    })
-    .catch(err => console.log(err));
+    useEffect(() => {
+        fetch(card.image)
+        .then(response => response.blob())
+        .then(blob => {
+            const localUrl = window.URL.createObjectURL(blob);
+            setUrl(localUrl);
+        })
+        .catch(err => console.log(err));
+    }, [])
+
     return (
         <div className="inventory-card">
             <div className="inventory-card-faces">
                 <div className="inventory-back-face" style={{backgroundImage: `url(${card.faction})`}}>
-                    <a className="download-link" href={url} download={card.cardname}>Download The Card Image</a>
+                    <a className="download-link" href={url} download={card.cardname + ".jpeg"}>Download The Card Image</a>
                 </div>
                 <div className="inventory-front-face" style={{backgroundImage: `url(${card.image})`}}>
                     <h4 className="inventory-card-power">{card.power}</h4>
